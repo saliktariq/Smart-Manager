9409266ae162948faaf7a04965a5057438e16c5f
Salik Tariq <saliktariq@icloud.com>
2021-01-09 20:56:36 +0000

    Added API key for SendGrid
    Added API key for SendGrid

---
+++ app/src/main/java/app/smartmanager/helper/ApiKeys.java
@@ -0,0 +1,15 @@
+package app.smartmanager.helper;
+
+public class ApiKeys {
+
+    //API key for SendGrid Email Service
+    private static  final String KEY = "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";
+
+    ApiKeys(){
+        super();
+    }
+    static String getKey(){
+        return KEY;
+    }
+
+}
