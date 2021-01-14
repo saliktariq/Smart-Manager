371c44bade4d674c147b448f1f73878ed8a80650
Salik Tariq <saliktariq@icloud.com>
2021-01-14 19:23:03 +0000

    HomeScreen
    homescreen layout started

---
+++ app/src/main/java/app/smartmanager/ui/home/HomeScreen.java
@@ -0,0 +1,38 @@
+package app.smartmanager.ui.home;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import app.smartmanager.R;
+
+public class HomeScreen extends Fragment {
+
+    private HomeScreenViewModel mViewModel;
+
+    public static HomeScreen newInstance() {
+        return new HomeScreen();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.home_screen_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(HomeScreenViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/app/smartmanager/ui/home/HomeScreenViewModel.java
@@ -0,0 +1,7 @@
+package app.smartmanager.ui.home;
+
+import androidx.lifecycle.ViewModel;
+
+public class HomeScreenViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
